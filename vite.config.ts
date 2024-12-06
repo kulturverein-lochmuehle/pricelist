7241a0afbd079c5e152754181650bc75a911c4a3
David Enke <david@kvlm.de>
2024-12-06 22:48:41 +0100

    chore: update deps and add linting

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,21 @@
-import type { UserConfig } from 'vite';
+import { defineConfig } from 'vite';
+import checker from 'vite-plugin-checker';
+import mkcert from 'vite-plugin-mkcert';
 
-export default {
+// Vite configuration
+// https://vite.dev/config/
+export default defineConfig(async () => ({
+  // ensure relative asset paths in index.html file
   base: './',
+  // browser targets
+  // https://vite.dev/config/build-options.html#build-target
   build: {
-    target: 'esnext',
+    target: ['es2020', 'edge120', 'firefox117', 'chrome120', 'safari17'],
   },
-} satisfies UserConfig;
+  plugins: [
+    // add type check directly to vite
+    checker({ typescript: true, overlay: false }),
+    // ssl für lokalen dev server (cors int api)
+    mkcert(),
+  ],
+}));
