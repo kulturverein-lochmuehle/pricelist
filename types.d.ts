a8f5d09e35b4a0f71fd171a976292e38f174591c
David Enke <david@kvlm.de>
2025-08-21 16:05:26 +0200

    chore: bring back stylelint

--- types.d.ts
+++
@@ -1,6 +0,0 @@
-declare module 'eslint-plugin-unused-imports' {
-  import type { ESLint } from 'eslint';
-
-  const eslintPluginUnusedImports: ESLint.Plugin;
-  export default eslintPluginUnusedImports;
-}
